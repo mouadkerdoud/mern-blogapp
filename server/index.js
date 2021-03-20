f6f329b11afa8fd64b06ad83e9cb1781d8a99c2e
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-03-20 20:56:09 +0100

    Established connection to the Mongo database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
- import express from "express"
- import bodyParser from "body-parser"
- import mongoose from "mongoose"
- import cors from "cors"
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+
+ //Express app
+ const app = express()
+
+ //General config
+ app.use(bodyParser.json({limit: "30mb", extended: true}))
+ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+ app.use(cors())
+
+ //Database
+ const CONNECTION_URL = "mongodb+srv://duodrek:imsopure123@cluster0.fh59a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ const PORT = process.env.PORT || 5000;
+
+ mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => app.listen(PORT, ()=> console.log(`Server running on port : ${PORT}`)) )
+    .catch(error => console.log(error.message) )
+
+mongoose.set("useFindAndModify", false)
