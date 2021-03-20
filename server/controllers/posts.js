58c5e7cb90a58a636f175eba4e2be20b060a3c91
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-03-20 21:53:19 +0100

    Implemented the logic to getting posts and adding posts in the posts controller and setting up the router for them

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,30 @@
+const PostMessage = require("../models/postMessage")
 
- const getPosts =  (req, res) => {
-    res.send("THIS WORKS")
+//Getting posts
+ const getPosts =  async (req, res) => {
+    try {
+        const posts = await PostMessage.find()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
 }
 
+
+//Creating a post
+const createPost = async (req, res) => {
+    const post = req.body
+    const newPost = new PostMessage(post)    
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+}
+
+
 module.exports = {
-    getPosts
+    getPosts,
+    createPost
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,7 +5,10 @@ const router = express.Router()
 const postsController = require("../controllers/posts") 
 
 
+
+//Routes
 router.get("/", postsController.getPosts)
+router.get("/", postsController.createPost)
 
 
 module.exports = router
